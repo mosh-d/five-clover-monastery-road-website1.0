c41e515bcf75d272c77ca8edb78b1ef464fb69f5
mosh-d <shopsy8@gmail.com>
2026-03-26 15:40:40 +0100

    Updating footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -195,7 +195,7 @@ export default function Footer() {
                 className="flex flex-col gap-[1rem] max-md:text-center"
               >
                 <a
-                  href="https://fiveclovermonastery.fivecloverhotels.com/"
+                  href="https://fivecloverhotelmonastery.com/"
                   target="_blank"
                   className="text-2xl"
                 >
@@ -232,28 +232,28 @@ export default function Footer() {
                 className="flex flex-col gap-[1rem] max-md:text-center"
               >
                 <a
-                  href="https://www.caritasinnigbobihotel.com/"
+                  href="https://www.igbobi.caritasinn.com/"
                   target="_blank"
                   className="text-2xl"
                 >
-                  Igbobo
+                  Igbobi
                 </a>
                 <a
-                  href="https://www.caritasinnilasanhotel.com/"
+                  href="https://www.ilasan.caritasinn.com/"
                   target="_blank"
                   className="text-2xl"
                 >
                   Ilasan
                 </a>
                 <a
-                  href="https://www.caritasinnlekkihotel.com/"
+                  href="https://www.lekki.caritasinn.com/"
                   target="_blank"
                   className="text-2xl"
                 >
                   Lekki, Phase 1
                 </a>
                 <a
-                  href="https://www.caritasinnyabahotel.com/"
+                  href="https://www.yaba.caritasinn.com/"
                   target="_blank"
                   className="text-2xl"
                 >
@@ -276,28 +276,35 @@ export default function Footer() {
                 className="flex flex-col gap-[1rem] max-md:text-center"
               >
                 <a
-                  href="https://ringrubyhotelsangotedo.com/"
+                  href="https://www.bateye.ringrubyhotel.com/"
                   target="_blank"
                   className="text-2xl"
                 >
-                  Sangotedo
+                  Bateye Road
                 </a>
                 <a
-                  href="https://ringrubyhotelesoikejagra.com/"
+                  href="https://www.unitedestate.ringrubyhotel.com/"
+                  target="_blank"
+                  className="text-2xl"
+                >
+                  United Estate
+                </a>
+                <a
+                  href="https://www.eso.ringrubyhotel.com/"
                   target="_blank"
                   className="text-2xl"
                 >
                   Eso close, ikeja GRA
                 </a>
                 <a
-                  href="https://ringrubyhoteloduduwaikejagra.com/"
+                  href="https://www.oduduwa.ringrubyhotel.com/"
                   target="_blank"
                   className="text-2xl"
                 >
                   Oduduwa way, ikeja GRA
                 </a>
                 <a
-                  href="https://ringrubyhoteloduduwaikejagra.com/"
+                  href="https://www.valuecounty.ringrubyhotel.com/"
                   target="_blank"
                   className="text-2xl"
                 >
